93a535aa9aaf72e3beec7a9fe91c20b51cc0cbc4
Henok Tesfamikael <henoktesfamikael16@gmail.com>
2025-01-08 10:13:31 +0300

    add APPLICATION_ROOT

--- config.py
+++ config.py
@@ -6,3 +6,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "app.db")
+    APPLICATION_ROOT = "/fos"
